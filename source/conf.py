2ff21dd059b2d0f1b329c350dcc099610a6a1c0b
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2020-06-16 15:36:13 +0200

    wip - dropping rtd theme

--- source/conf.py
+++ source/conf.py
@@ -48,8 +48,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+
+import sphinx_kr_theme
+html_theme_path = [sphinx_kr_theme.get_html_theme_path()]
+html_theme = 'kr'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -58,28 +60,11 @@ html_static_path = ['_static']
 
 html_logo = '_static/images/kobologo.svg'
 
-# See: https://sphinx-rtd-theme.readthedocs.io
-html_theme_options = {
-    # 'canonical_url': '',
-    # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
-    'logo_only': True,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': True,
-    'vcs_pageview_mode': '',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+# We override all css
+# html_style = 'css/kobo_theme.css'
 
-# We override all css, using a customized SASS sources from sphinx_rtd_theme
-html_style = 'css/kobo_theme.css'
-
-# # These paths are either relative to html_static_path
-# # or fully qualified paths (eg. https://...)
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
 # html_css_files = [
 #     'css/custom.css',
 # ]
@@ -87,6 +72,6 @@ html_style = 'css/kobo_theme.css'
 #     'js/custom.js',
 # ]
 
-# html_sidebars = {
-#     "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
-# }
+html_sidebars = {
+    "**": ["globaltoc.html", "localtoc.html", "searchbox.html"]
+}
