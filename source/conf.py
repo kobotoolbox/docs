fd99889d2aeb17b7573af095c3b2cf3155184cb8
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2020-06-16 13:36:51 +0200

    wip - building custom theme

--- source/conf.py
+++ source/conf.py
@@ -49,50 +49,44 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_material'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# html theme options from https://pypi.org/project/sphinx-material/
+html_logo = '_static/images/kobologo.svg'
 
+# See: https://sphinx-rtd-theme.readthedocs.io
 html_theme_options = {
-
-    # Set the name of the project to appear in the navigation.
-    'nav_title': 'KoBoToolbox Documentation',
-
-    # Set you GA account ID to enable tracking
-    # 'google_analytics_account': 'UA-XXXXX',
-
-    # Specify a base_url used to generate sitemap.xml. If not
-    # specified, then no sitemap will be built.
-    #'base_url': 'https://project.github.io/project',
-
-    # Set the color and the accent color
-    'color_primary': '414453',
-    'color_accent': '2095f3',
-    'theme_color': '414453',
-    # Set the repo location to get a badge with stats
-    # 'repo_url': 'https://github.com/kobotoolbox/docs/',
-    #'repo_name': 'KoBoToolbox documentation',
-
-    # Visible levels of the global TOC; -1 means unlimited
-    'globaltoc_depth': -1,
-    # If False, expand all TOC entries
-    'globaltoc_collapse': False,
-    # If True, show hidden TOC entries
-    'globaltoc_includehidden': True,
-    
-    'logo_icon': '&#xe869',
-    # 'html_minify': True,
-    # 'css_minify': True,
-    # 'base_url': 'http://bashtage.github.io/sphinx-material/',
-
-
+    # 'canonical_url': '',
+    # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    'vcs_pageview_mode': '',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
 
-html_sidebars = {
-    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
-}
+# We override all css, using a customized SASS sources from sphinx_rtd_theme
+html_style = 'css/kobo_theme.css'
+
+# # These paths are either relative to html_static_path
+# # or fully qualified paths (eg. https://...)
+# html_css_files = [
+#     'css/custom.css',
+# ]
+# html_js_files = [
+#     'js/custom.js',
+# ]
+
+# html_sidebars = {
+#     "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+# }
