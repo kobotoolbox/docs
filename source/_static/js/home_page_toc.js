f6eefe9eb95f80ca82424e606905df551ad5423e
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2020-07-21 16:16:14 +0200

    fix home page TOCs

--- source/_static/js/home_page_toc.js
+++ source/_static/js/home_page_toc.js
@@ -1,29 +1,39 @@
 (function () {
-  function limitTocDisplay(tocEl) {
-    const exceedingChildren = tocEl.querySelectorAll("li.toctree-l1:nth-child(n+6)");
-
-    if (exceedingChildren.length > 0) {
-      // hide all items over 5
-      exceedingChildren.forEach(hide);
-
-      // add show all button
-      const showAll = window.document.createElement("div");
-      showAll.classList.add("show-all-button");
-      showAll.classList.add("link");
-      showAll.insertAdjacentText("beforeend", "See all");
-      showAll.addEventListener("click", function () {
-        // on click show all previously hidden items and hide the button
-        exceedingChildren.forEach(show);
-        hide(showAll);
+  function findSection(tocEl) {
+    let foundSection = null;
+    if (window.docs && window.docs.sectionsData && tocEl) {
+      Object.keys(window.docs.sectionsData).forEach((key) => {
+        const item = window.docs.sectionsData[key];
+        if (
+          tocEl.getAttribute("id") !== null &&
+          `home-page-toc-${item.name}` === tocEl.getAttribute("id")
+        ) {
+          foundSection = item;
+        }
       });
+    }
+    return foundSection;
+  }
+
+  function setupToc(tocEl) {
+    const section = findSection(tocEl);
+
+    if (section && window.docs && window.docs.rootUrl) {
+      // add show all button
+      const showAll = window.document.createElement("a");
+      showAll.classList.add("show-all-button");
+      showAll.insertAdjacentText("beforeend", "See all");
+      showAll.href = window.docs.rootUrl + `${section.name}.html`;
       tocEl.appendChild(showAll);
+    } else {
+      console.warn("section not found", tocEl)
     }
   }
 
   window.addEventListener("load", function () {
     const tocs = document.querySelectorAll(".toctree-wrapper[id^='home-page-toc-']");
     if (tocs.length !== 0) {
-      tocs.forEach(limitTocDisplay)
+      tocs.forEach(setupToc)
       console.info("home page TOCs ready");
     }
   });
