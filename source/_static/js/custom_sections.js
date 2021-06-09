894fd568458b746f0a1bcb5c76f5e09d8f9a8989
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-06-10 00:42:44 +0200

    fix missing caption classname sphinx bug

--- source/_static/js/custom_sections.js
+++ source/_static/js/custom_sections.js
@@ -1,6 +1,15 @@
 (function () {
   window.addEventListener("DOMContentLoaded", function () {
-    const captions = document.querySelectorAll(".custom-section-page .page__body > p.caption");
+    let captions = document.querySelectorAll(".custom-section-page .page__body > p.caption");
+
+    // HACK FIX WTF: previously we were targeting p.caption, but for some reason
+    // on production build the caption class name is not added. We add it.
+    if (captions.length === 0) {
+      captions = document.querySelectorAll(".custom-section-page .page__body > p");
+      captions.forEach((target) => {
+        target.classList.add('caption');
+      });
+    }
 
     if (captions.length !== 0) {
       for (let i = 0; i < captions.length; i++) {
@@ -10,7 +19,7 @@
           child.setAttribute("id", `${window.docs.currentPagename}-section-caption`);
         }
       }
-      console.info("custom section page ready");
+      console.info("Custom section page ready.");
     }
   });
 })();
